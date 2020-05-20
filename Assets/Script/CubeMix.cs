129af685033e23d8b67783187506f85ccc94f40e
juyeonjuyeonjuyeon <lucifelu4325@gmail.com>
2020-05-20 16:57:41 +0900

    수정
    - 애니메이션 스크립트 예제 추가
    - 파일 정리

---
+++ Assets/Script/CubeMix.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMix : MonoBehaviour
+{
+    Animation ani;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayAnim(int num)
+    {
+        switch (num)
+        {
+            case 1: ani.Play("Cube_Mix1"); return;
+            case 2: ani.Play("Cube_Mix2"); return;
+            case 3: 
+                if(ani.IsPlaying("Cube_Mix1")) ani.Blend("Cube_Mix2");
+                else ani.Blend("Cube_Mix1");
+                return;
+            default: ani.Stop(); return;
+        }
+    }
+}
