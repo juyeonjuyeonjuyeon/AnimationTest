4d3f749c5156d8fc5d500cf4d9e86492c2b81054
juyeonjuyeonjuyeon <39845826+juyeonjuyeonjuyeon@users.noreply.github.com>
2020-05-22 18:10:48 +0900

    *씬 위치 수정 *레이어 테스트 추가 *이벤트 테스트 추가

---
+++ Assets/CylinderEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CylinderEvent : MonoBehaviour
+{
+    public Text label;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeText(string value)
+    {
+        label.text = value;
+    }
+
+    public void RotateCylinder()
+    {
+
+    }
+}
---
+++ Assets/Script/CubeProperty.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeProperty : MonoBehaviour
+{
+    public float m_FloatType;
+    public Color m_Color;
+    public Vector2 m_Vec2;
+    public Vector3 m_Vec3;
+    public Vector4 m_Vec4;
+    public Quaternion m_Quaternion;
+    public bool m_Bool;
+    private float p_FloatType;
+    private Color p_Color;
+    private Vector2 p_Vec2;
+    private Vector3 p_Vec3;
+    private Vector4 p_Vec4;
+    private Quaternion p_Quaternion;
+    private bool p_Bool;
+    public GameObject label;
+    Text t;
+
+    public float FloatType
+    {
+        get { return p_FloatType; }
+        set { p_FloatType = value; }
+    }
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = label.GetComponent<Text>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       t.color = m_Color;
+    }
+}
